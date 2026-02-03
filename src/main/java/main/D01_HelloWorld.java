37a83ee83dde42d6ad11c25964a9ea33390d7290
nameless_programm <zelos.games69@gmail.com>
2026-02-03 10:56:31 +0100

    change to D01_Hello_World

---
+++ src/main/java/main/D01_HelloWorld.java
@@ -0,0 +1,15 @@
+package main;
+
+public class D01_HelloWorld {
+
+	public static void main(String[] args) {
+		String text; // Deklaration
+		
+		text = "Hello Wolrd"; // Initialisierung
+		
+		/* Ausgabe */
+		System.out.println(text);
+
+	}
+
+}
